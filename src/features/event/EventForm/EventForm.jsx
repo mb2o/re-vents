6c8995b55871894652ccff36aaa0203294cc4795
mb2o <mboomaars@gmail.com>
2019-08-24 10:59:39 +0200

    Controlled Forms

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -2,31 +2,77 @@ import React, { Component } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 
 class EventForm extends Component {
+   state = {
+      title: "",
+      date: "",
+      city: "",
+      venue: "",
+      hostedBy: ""
+   };
+
+   handleFormSubmit = (event) => {
+      event.preventDefault();
+      console.log(this.state);
+   };
+
+   handleInputChange = ({ target: { name, value } }) => {
+      this.setState({
+         [name]: value
+      });
+   };
+
    render() {
       const { cancelFormOpen } = this.props;
+      const { title, date, city, venue, hostedBy } = this.state;
 
       return (
          <Segment>
-            <Form>
+            <Form onSubmit={this.handleFormSubmit} autoComplete='off'>
                <Form.Field>
                   <label>Event Title</label>
-                  <input placeholder='First Name' />
+                  <input
+                     placeholder='Event Title'
+                     name='title'
+                     value={title}
+                     onChange={this.handleInputChange}
+                  />
                </Form.Field>
                <Form.Field>
                   <label>Event Date</label>
-                  <input type='date' placeholder='Event Date' />
+                  <input
+                     type='date'
+                     name='date'
+                     placeholder='Event Date'
+                     value={date}
+                     onChange={this.handleInputChange}
+                  />
                </Form.Field>
                <Form.Field>
                   <label>City</label>
-                  <input placeholder='City event is taking place' />
+                  <input
+                     placeholder='City event is taking place'
+                     name='city'
+                     value={city}
+                     onChange={this.handleInputChange}
+                  />
                </Form.Field>
                <Form.Field>
                   <label>Venue</label>
-                  <input placeholder='Enter the Venue of the event' />
+                  <input
+                     placeholder='Enter the Venue of the event'
+                     name='venue'
+                     value={venue}
+                     onChange={this.handleInputChange}
+                  />
                </Form.Field>
                <Form.Field>
                   <label>Hosted By</label>
-                  <input placeholder='Enter the name of person hosting' />
+                  <input
+                     placeholder='Enter the name of person hosting'
+                     name='hostedBy'
+                     value={hostedBy}
+                     onChange={this.handleInputChange}
+                  />
                </Form.Field>
                <Button positive type='submit'>
                   Submit
--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -18,9 +18,9 @@ class EventListItem extends Component {
                         src={event.hostPhotoURL}
                      />
                      <Item.Content>
-                        <Item.Header as='a'>{event.title}</Item.Header>
+                        <Item.Header>{event.title}</Item.Header>
                         <Item.Description>
-                           Hosted by <a>{event.hostedBy}</a>
+                           Hosted by {event.hostedBy}
                         </Item.Description>
                      </Item.Content>
                   </Item>
