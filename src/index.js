82030f5cba4848e75df4f975ad44612a14cdd60e
mb2o <mboomaars@gmail.com>
2019-08-25 10:39:44 +0200

    Redux store configuration

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
 
 import "./index.css";
+import { configureStore } from "./app/store/configureStore";
+
+const store = configureStore();
+console.log(store.getState());
 
 const rootEl = document.getElementById("root");
 
 let render = () => {
    ReactDOM.render(
-      <BrowserRouter>
-         <App />
-      </BrowserRouter>,
+      <Provider store={store}>
+         <BrowserRouter>
+            <App />
+         </BrowserRouter>
+      </Provider>,
       rootEl
    );
 };
