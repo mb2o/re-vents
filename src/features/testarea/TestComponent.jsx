c3018cd4b3289a65b1d5d514ed8f2317ae04268b
mb2o <mboomaars@gmail.com>
2019-08-25 10:59:18 +0200

    Creating our first actions

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,19 +1,40 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Button } from "semantic-ui-react";
+
+import { incrementCounter, decrementCounter } from "./testActions";
 
 const mapStateToProps = (state) => ({
    data: state.data
 });
 
+const mapDispatchToProps = {
+   incrementCounter,
+   decrementCounter
+};
+
 class TestComponent extends Component {
    render() {
+      const { data, incrementCounter, decrementCounter } = this.props;
+
       return (
          <div>
             <h1>Test Component</h1>
-            <h3>The answer is: {this.props.data}</h3>
+            <h3>The answer is: {data}</h3>
+            <Button
+               onClick={incrementCounter}
+               positive
+               content='Increment'></Button>
+            <Button
+               onClick={decrementCounter}
+               negative
+               content='Decrement'></Button>
          </div>
       );
    }
 }
 
-export default connect(mapStateToProps)(TestComponent);
+export default connect(
+   mapStateToProps,
+   mapDispatchToProps
+)(TestComponent);
