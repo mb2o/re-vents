da1c88bffae2713dc275ecb7bb2193f5c2b7db0e
mb2o <mboomaars@gmail.com>
2019-08-28 20:16:35 +0200

    Adding the Photos page

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
    authDomain: "revents-2ad3f.firebaseapp.com",
    databaseURL: "https://revents-2ad3f.firebaseio.com",
    projectId: "revents-2ad3f",
-   storageBucket: "",
+   storageBucket: "revents-2ad3f.appspot.com",
    messagingSenderId: "354112880728",
    appId: "1:354112880728:web:ea30172ea973af78"
 };
--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -1,11 +1,60 @@
-import React from "react";
+import {
+   Button,
+   Card,
+   Divider,
+   Grid,
+   Header,
+   Image,
+   Segment
+} from "semantic-ui-react";
+import React, { Component } from "react";
 
-const PhotosPage = () => {
-   return (
-      <div>
-         <h1>PhotosPage</h1>
-      </div>
-   );
-};
+class PhotosPage extends Component {
+   render() {
+      return (
+         <Segment>
+            <Header content='Your Photos' dividing size='large' />
+            <Grid>
+               <Grid.Row />
+               <Grid.Column width={4}>
+                  <Header color='teal' content='Step 1 - Add Photo' sub />
+               </Grid.Column>
+               <Grid.Column width={1} />
+               <Grid.Column width={4}>
+                  <Header color='teal' content='Step 2 - Resize image' sub />
+               </Grid.Column>
+               <Grid.Column width={1} />
+               <Grid.Column width={4}>
+                  <Header
+                     color='teal'
+                     content='Step 3 - Preview and Upload'
+                     sub
+                  />
+               </Grid.Column>
+            </Grid>
+
+            <Divider />
+            <Header color='teal' content='All Photos' sub />
+
+            <Card.Group itemsPerRow={5}>
+               <Card>
+                  <Image src='https://randomuser.me/api/portraits/men/20.jpg' />
+                  <Button positive>Main Photo</Button>
+               </Card>
+
+               <Card>
+                  <Image src='https://randomuser.me/api/portraits/men/20.jpg' />
+                  <div className='ui two buttons'>
+                     <Button basic color='green'>
+                        Main
+                     </Button>
+                     <Button basic color='red' icon='trash' />
+                  </div>
+               </Card>
+            </Card.Group>
+         </Segment>
+      );
+   }
+}
 
 export default PhotosPage;
