485fee0eaf6766bdb91ac4c94f69a9d415d527b1
mb2o <mboomaars@gmail.com>
2019-09-01 12:01:02 +0200

    React lifecycle method - componentWillUnmount

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -79,6 +79,11 @@ class EventForm extends Component {
       await firestore.setListener(`events/${match.params.id}`);
    }
 
+   async componentWillUnmount() {
+      const { firestore, match } = this.props;
+      await firestore.unsetListener(`events/${match.params.id}`);
+   }
+
    onFormSubmit = async (values) => {
       values.venueLatLng = this.state.venueLatLng;
 
